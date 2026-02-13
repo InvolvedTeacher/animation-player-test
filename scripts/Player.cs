7447e91edea8c18e2bbd7c559a76131eab927eb7
InvolvedTeacher <involvedteacher.dev@gmail.com>
2026-02-13 11:45:55 +0100

    Add input mapping and behavior for movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,39 +3,30 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float speed = 300.0f;
+	public const float jump_velocity = -800.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
 		}
 		else
 		{
-			velocity = new Vector2(velocity.X, -800);
+			velocity = new Vector2(velocity.X, jump_velocity);
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		float direction = Input.GetAxis("left", "right");
+		if (direction != 0)
 		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction * speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
 		}
 
 		Velocity = velocity;
